65c971a0aba833debd32af3ad0d7658dcdd8f42b
Divam <dfordivam@gmail.com>
2022-01-31 14:33:25 +0900

    Add code to test callbacks

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -3,10 +3,12 @@
 
 module Frontend where
 
+import Control.Lens hiding ((#))
 import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Language.Javascript.JSaddle (eval, liftJSM)
+import Language.Javascript.JSaddle hiding (catch)
 
 import Obelisk.Frontend
 import Obelisk.Configs
@@ -37,11 +39,56 @@ frontend = Frontend
       -- print "Hello, World!" on the client.
       prerender_ blank $ liftJSM $ void $ eval ("console.log('Hello, World!')" :: T.Text)
 
-      elAttr "img" ("src" =: $(static "obelisk.jpg")) blank
-      el "div" $ do
-        exampleConfig <- getConfig "common/example"
-        case exampleConfig of
-          Nothing -> text "No config file found in config/common/example"
-          Just s -> text $ T.decodeUtf8 s
+      prerender_ blank $ do
+        ev <- button "Click"
+        performEvent_ $ ffor ev $ \_ -> do
+          liftJSM runCallbacks
       return ()
   }
+
+
+runCallbacks = do
+  w <- jsg ("window" :: String)
+  o <- create
+  c <- jsg ("console" :: String)
+  s1 <- toJSVal ("1example string" :: String)
+  s2 <- toJSVal ("2example string" :: String)
+  s3 <- toJSVal ("3example string" :: String)
+  let consoleLog t = void $
+        c # ("log" :: String) $ ([t] :: [String])
+  let callback1 = fun $ \_ _ _ -> do
+        -- consoleLog "Executing Callback 1"
+        -- res2 <- valToText s1
+        -- eval ("window.myObjName.hsCallback2()" :: String)
+        -- consoleLog (T.unpack res2)
+        res <- eval ("someUndefinedAPI();" :: String)
+        -- res3 <- valToText s3
+        pure ()
+        -- consoleLog (T.unpack res3)
+        -- consoleLog "callback 1 done"
+      hsCallback1 = "hsCallback1" :: String
+  let callback2 = fun $ \_ _ _ -> do
+        -- consoleLog "Executing callback 2"
+        res3 <- valToText s2
+        consoleLog (T.unpack res3)
+        pure ()
+      hsCallback2 = "hsCallback2" :: String
+      objName = "myObjName" :: String
+      apiName = "myApiName" :: String
+
+  (o <# hsCallback1) callback1
+  (o <# hsCallback2) callback2
+
+  (w <# objName) o
+
+  eval ("window.setTimeout (\
+    \(function() {\
+    \  console.log('before callback 1'); \
+    \  try { window.myObjName.hsCallback1(); } catch (e) {return;};\
+    \  console.log('after callback 1'); \
+    \  for (let i = 0; i < 1; i++) { window.myObjName.hsCallback2(); }\
+    \  console.log('after callback 2'); \
+    \}), 0)" :: String)
+
+
+  pure ()
